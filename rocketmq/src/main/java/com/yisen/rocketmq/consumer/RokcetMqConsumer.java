bbea26d2f1cc2a41d4f32bb06d0239205ebed42f
liangyisen <liangyisende@gmail.com>
2021-04-09 17:51:59 +0800

    新增rocketmq demo

---
+++ rocketmq/src/main/java/com/yisen/rocketmq/consumer/RokcetMqConsumer.java
@@ -0,0 +1,57 @@
+package com.yisen.rocketmq.consumer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.io.UnsupportedEncodingException;
+
+public class RokcetMqConsumer {
+
+    private static final String namesrvAddr = "localhost:9876";
+
+    private final static String TOPIC_TEST = "TOPIC_TEST";
+
+    private final static String TAG_TEST = "TAG_TEST";
+
+    private final DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer1");
+
+    public static void main(String[] args) throws MQClientException {
+        RokcetMqConsumer rrokcetMqConsumer = new RokcetMqConsumer();
+
+        rrokcetMqConsumer.consumer.setNamesrvAddr(namesrvAddr);
+        rrokcetMqConsumer.consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        //设置广播消费模式
+        rrokcetMqConsumer.consumer.setMessageModel(MessageModel.BROADCASTING);
+
+        //订阅主题
+        rrokcetMqConsumer.consumer.subscribe(TOPIC_TEST, TAG_TEST);
+
+        //注册消息监听器
+        rrokcetMqConsumer.consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            for (MessageExt msg : msgs) {
+                byte[] body = msg.getBody();
+                String messageBody = null;
+                try {
+                    messageBody = new String(body, RemotingHelper.DEFAULT_CHARSET);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("消费Queue = " + messageBody);
+            }
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+
+        //启动消费端
+        rrokcetMqConsumer.consumer.start();
+
+    }
+}
+
---
+++ rocketmq/src/main/java/com/yisen/rocketmq/provider/RocketmqProviderDemo.java
@@ -0,0 +1,85 @@
+package com.yisen.rocketmq.provider;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import javax.annotation.PreDestroy;
+
+public class RocketmqProviderDemo {
+    //nameserver地址
+    private final static String namesrvAddr = "localhost:9876";
+
+    private final DefaultMQProducer producer = new DefaultMQProducer("ProducerTest");
+
+    private final static String TOPIC_TEST = "TOPIC_TEST";
+
+    private final static String TAG_TEST = "TAG_TEST";
+
+
+    /**
+     * 初始化
+     */
+    public void start() {
+        try {
+            System.out.println("MQ：启动ProducerTest生产者");
+            producer.setNamesrvAddr(namesrvAddr);
+            producer.start();
+            //发送消息
+            for (int i = 0; i < 100; i++) {
+                sendMessage("hello mq" + i);
+            }
+        } catch (MQClientException e) {
+            System.out.println("MQ：启动ProducerTest生产者失败：" + e.getResponseCode() + e.getErrorMessage());
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    public void sendMessage(String data) {
+        System.out.println("MQ: 生产者发送消息 :{}" + data);
+        Message message = null;
+        try {
+            //转换成字符数组
+            byte[] messageBody = data.getBytes(RemotingHelper.DEFAULT_CHARSET);
+            //创建消息对象
+            message = new Message(TOPIC_TEST, TAG_TEST, String.valueOf(System.currentTimeMillis()), messageBody);
+            //同步发送消息
+            SendResult sendResult = producer.send(message);
+            System.out.println("topic: " + TOPIC_TEST + " | tag:" + TAG_TEST + "| sendResult: " + sendResult);
+            //异步发送消息
+            /*producer.send(message, new SendCallback() {
+                public void onSuccess(SendResult sendResult) {
+                    System.out.println("MQ: CouponProducer生产者发送消息" + sendResult);
+                }
+
+                public void onException(Throwable throwable) {
+                    System.out.println(throwable.getMessage() +  throwable);
+                }
+            });*/
+            //单向发送 只发送消息，不等待服务器响应，只发送请求不等待应答。
+            //producer.sendOneway(message);
+        } catch (Exception e) {
+            if (message != null) {
+                System.out.println("producerGroup:ProducerTest,Message:{}" + JSON.toJSON(message));
+            }
+            e.printStackTrace();
+        }
+    }
+
+    @PreDestroy
+    public void stop() {
+        if (producer != null) {
+            producer.shutdown();
+            System.out.println("MQ：关闭ProducerTest生产者");
+        }
+    }
+
+    public static void main(String[] args) {
+        RocketmqProviderDemo rocketmqDemo = new RocketmqProviderDemo();
+        rocketmqDemo.start();
+    }
+
+}
