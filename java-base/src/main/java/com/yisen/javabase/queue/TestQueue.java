e2f9bea6dd131b032f3c026b3f53805a9591c2c3
ext.liangyisen3 <ext.liangyisen3@jd.com>
2023-11-15 22:02:38 +0800

    测试LinkedBlockingQueue

--- java-base/src/main/java/com/yisen/javabase/queue/TestQueue.java
+++ java-base/src/main/java/com/yisen/javabase/queue/TestQueue.java
@@ -1,8 +1,10 @@
 package com.yisen.javabase.queue;
 
-import java.util.Arrays;
+import org.junit.Test;
+
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class TestQueue {
 
@@ -19,6 +21,48 @@ public class TestQueue {
         int i = strings.remainingCapacity();
         System.out.println(i);
         String take = strings.take();
+
+    }
+
+    /**
+     * 测试LinkedBlockingQueue
+     */
+    @Test
+    public void testLinkedBlockingQueue() {
+        BlockingQueue<Object> objects = new LinkedBlockingQueue<>();
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    Object take = objects.take();
+                    System.out.println("take = " + take);
+                    //
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("e = " + e);
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    Thread.sleep(1000);
+                    objects.put("1");
+                    System.out.println("put = " + 1);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        //主线程阻塞
+        try {
+            Thread.sleep(1011100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
